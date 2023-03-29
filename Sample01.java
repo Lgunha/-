40e7acb712cf46bcb4636b2c9d094fd5442cf48e
Lgunha <101403982+Lgunha@users.noreply.github.com>
2023-03-29 13:32:23 +0900

    Add files via upload

---
+++ Sample01.java
@@ -0,0 +1,25 @@
+package test1;
+
+public class Sample01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 주석 ... 개발자가 안내문구, 개발하면서 설명 들을 기록..
+		// 프로젝트는 패키지 단위로 소스를 관리한다. 자바소스는 class단위로 만든다.
+		// 자바소소의 파일이름의 의미는 클래스면.java이다.
+		
+		System.out.println("hello world!!");
+		// sysout ctrl + spacebar
+		// 실행은  ctrl + f11
+		
+		System.out.println("나는 건하야");
+		
+		//test3 vozlwldp sample02 자바파일 생성후
+		//System.out.println("안녕자바!!"); 명령어 작성 후 실행시키시오
+		
+		
+		
+		
+	}
+
+}
